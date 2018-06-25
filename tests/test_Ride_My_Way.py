c038c781a4e07ac4a6f16b00ae781722215f8fae
Abdulaziz Mohammed <abdulmoha786@gmail.com>
2018-06-25 10:53:42 +0300

    [chore #158568987] Add tests methods in the test case

---
+++ tests/test_Ride_My_Way.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+class RideMyWayTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_user_generates_auth_token(self):
+        pass
+
+    def test_user_signup(self):
+        pass
+
+    def test_user_login(self):
+        pass
+
+    def test_login_fails_for_incorrect_credentials(self):
+        pass
+
+    def test_duplicate_account_creation_fails(self):
+        pass
+
+    def test_user_can_create_ride_offer(self):
+        pass
+
+    def test_user_can_reschedule_ride_offer(self):
+        pass
+
+    def test_user_can_request_ride_offer(self):
+        pass
+
+    def test_user_can_send_friend_request(self):
+        pass
+
+    def test_user_can_respond_to_friend_request(self):
+        pass
+
+    def test_user_can_cancel_ride_request(self):
+        pass
+
+    def test_user_can_view_their_ride_requests(self):
+        pass
+
+    def test_user_can_view_their_ride_offers(self):
+        pass
